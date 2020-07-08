e1b18af73b1d6526ea5d542d161d4ad00edde842
Han Lin Yap <codler@users.noreply.github.com>
2020-07-08 15:52:06 +0200

    Add Socialstyrelsen

--- download_fhm.tsx
+++ download_fhm.tsx
@@ -38,3 +38,42 @@ request(
     );
   }
 );
+
+request(
+  "https://www.socialstyrelsen.se/globalassets/1-globalt/covid-19-statistik/statistik-over-antal-avlidna-i-covid-19/statistik-covid19-avlidna.xlsx",
+  { encoding: "binary" },
+  function (error, response, body) {
+    fs.writeFile(
+      "./socialstyrelsen/statistik-covid19-avlidna_latest.xlsx",
+      body,
+      "binary",
+      async function (err) {
+        let rows = await readXlsxFile(
+          "./socialstyrelsen/statistik-covid19-avlidna_latest.xlsx",
+          { sheet: 9 }
+        );
+
+        const date = new Date(
+          rows[1][0].replace(
+            "Avlidna i covid-19 enligt dödsorsaksintyg inkomna till den",
+            ""
+          )
+        );
+
+        if (isNaN(date.getTime())) {
+          console.log(new Error("Invalid date"));
+          process.exit(1);
+        }
+
+        const dateText = formattedDayDate(date);
+
+        fs.writeFile(
+          "./socialstyrelsen/statistik-covid19-avlidna_" + dateText + ".xlsx",
+          body,
+          "binary",
+          () => true
+        );
+      }
+    );
+  }
+);

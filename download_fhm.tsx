08d63e293d59e16da318a83f599a349a0d520720
Han Lin Yap <codler@users.noreply.github.com>
2022-03-24 20:00:23 +0100

    Fix vaccine date

--- download_fhm.tsx
+++ download_fhm.tsx
@@ -74,7 +74,7 @@ request(
           { getSheets: true }
         );
 
-        const date = new Date(fixDateMonthLocale(sheets[9].name));
+        const date = new Date(fixDateMonthLocale(sheets[11].name));
 
         if (isNaN(date.getTime())) {
           console.log(
